585c8f610505db25dbe7ef8dda26fedd25b79a33
Ahmed Fasih <wuzzyview@gmail.com>
2016-09-11 23:19:28 -0400

    Removed modal/view stuff.

--- spec/papyrus-sedge-view-spec.js
+++
@@ -1,7 +0,0 @@
-'use babel';
-
-import PapyrusSedgeView from '../lib/papyrus-sedge-view';
-
-describe('PapyrusSedgeView', () => {
-  it('has one valid test', () => { expect('life').toBe('easy'); });
-});
