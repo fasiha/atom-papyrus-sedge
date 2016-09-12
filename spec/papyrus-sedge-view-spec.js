5154491bb5ab4b164dbe7cd075b21f42e9c9e60f
Ahmed Fasih <wuzzyview@gmail.com>
2016-09-11 22:42:25 -0400

    Bare project

---
+++ spec/papyrus-sedge-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PapyrusSedgeView from '../lib/papyrus-sedge-view';
+
+describe('PapyrusSedgeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
